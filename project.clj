18e224b15a79fac9a8724e29c2dca4b756889949
James Reeves <jreeves@weavejester.com>
2019-12-14 15:18:49 +0000

    Update project URL and description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject hashp "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A better \"prn\" for debugging"
+  :url "https://github.com/weavejester/hashp"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]
