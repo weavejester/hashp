daabd031a2cc16b53e9f7bd8133e95bc14458727
James Reeves <jreeves@weavejester.com>
2019-12-14 03:27:24 +0000

    Add function name and line number

--- src/hashp/core.clj
+++ src/hashp/core.clj
@@ -1,14 +1,28 @@
 (ns hashp.core
   (:require [puget.printer :as puget]
+            [puget.color.ansi :as color]
             [clj-stacktrace.core :as stacktrace]))
 
 (def lock (Object.))
+
+(def prefix (color/sgr "#p" :red))
+
+(defn current-stacktrace []
+  (->> (.getStackTrace (Thread/currentThread))
+       (drop 3)
+       (stacktrace/parse-trace-elems)))
+
+(defn trace-str [trace]
+  (when-let [t (first (filter :clojure trace))]
+    (str "[" (:ns t) "/" (:fn t) ":" (:line t) "]")))
 
 (defn p* [form]
   `(let [result# ~form]
      (locking lock
        (println
-        (str (when-not (= result# '~form)
+        (str prefix
+             (color/sgr (trace-str (current-stacktrace)) :green) " "
+             (when-not (= result# '~form)
                (str (puget/cprint-str '~form) " => "))
              (puget/cprint-str result#)))
        result#)))
