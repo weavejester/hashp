d4d0cc61cad36d811339840f7a8f393ed468bc60
James Reeves <jreeves@weavejester.com>
2019-12-14 15:10:58 +0000

    Change license to MIT

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject hashp "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [mvxcvi/puget "1.2.0"]
                  [clj-stacktrace "0.2.8"]])
