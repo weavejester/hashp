9a8cf10e09e11b38f02beb5bbbaba0ef2ee6098e
James Reeves <jreeves@weavejester.com>
2020-01-14 17:53:50 +0000

    Add rrb-vector dependency (fixes #3)

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/core.rrb-vector "0.1.1"]
                  [mvxcvi/puget "1.2.0"]
                  [clj-stacktrace "0.2.8"]])
