00f4fdf53dcd21d4d8c30ad6c1573f57c59f8aa1
James Reeves <jreeves@weavejester.com>
2020-05-22 03:30:27 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/core.rrb-vector "0.1.1"]
-                 [mvxcvi/puget "1.2.0"]
+                 [mvxcvi/puget "1.3.0"]
                  [clj-stacktrace "0.2.8"]
                  [net.cgrand/macrovich "0.2.1"]
-                 [zprint "0.5.3"]])
+                 [zprint "0.5.4"]])
