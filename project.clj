c802ee82834f2cf90cf6186b5bce8f2948e42561
James Reeves <jreeves@weavejester.com>
2025-02-19 00:02:02 +0000

    Release 0.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dev.weavejester/hashp "0.2.2"
+(defproject dev.weavejester/hashp "0.3.0"
   :description "A better \"prn\" for debugging"
   :url "https://github.com/weavejester/hashp"
   :license {:name "The MIT License"
