a87683d5446711cb9525db52f95c0f037ca8a0e0
James Reeves <jreeves@weavejester.com>
2025-10-25 16:11:05 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/weavejester/hashp"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.12.0"]
+  :dependencies [[org.clojure/clojure "1.12.3"]
                  [org.clojure/core.rrb-vector "0.2.0"]
                  [mvxcvi/puget "1.3.4"]
                  [clj-stacktrace "0.2.8"]])
